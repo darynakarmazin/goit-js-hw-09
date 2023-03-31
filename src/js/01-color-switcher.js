77ac0d112afaec61a26880735a14993a0053fb3c
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-03-31 14:17:14 +0300

    Add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const bodyEl = document.querySelector('body');
 
+const buttonStart = document.querySelector('button[data-start]');
+
+const buttonStop = document.querySelector('button[data-stop]');
+
+buttonStart.addEventListener('click', onChangeColorBody);
+buttonStop.addEventListener('click', offChangeColorBody);
+let timerId = null;
+
+function onChangeColorBody(event) {
+  timerId = setInterval(changeColor, 1000);
+
+  buttonStart.setAttribute('disabled', 'disabled');
+  buttonStop.removeAttribute('disabled', 'disabled');
+}
+
+function offChangeColorBody(event) {
+  clearInterval(timerId);
+  buttonStop.setAttribute('disabled', 'disabled');
+  buttonStart.removeAttribute('disabled', 'disabled');
+}
+
+function changeColor(event) {
+  let RandomHexColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = RandomHexColor;
+}
